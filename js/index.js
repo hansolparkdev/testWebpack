6238046e8f4a78219a46a2042b714f3976f39148
hansolparkdev <solidev90@gmail.com>
2019-04-29 15:58:09 +0900

    send webpack

---
+++ js/index.js
@@ -0,0 +1,8 @@
+import moment from 'moment'
+
+function currentTime(){
+  return moment().format('H:m:s')
+}
+const div = document.createElement('div')
+document.body.appendChild(div)
+setInterval(() => div.innerText = currentTime(), 1000)
